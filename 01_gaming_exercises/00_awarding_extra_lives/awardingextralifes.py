1d99052522463c499bdbc391d1a620284e35affa
anthonyp0 <s11928611@students.duvalschools.org>
2024-09-12 13:04:47 -0400

    v0.2
    F strings

--- 01_gaming_exercises/00_awarding_extra_lives/awardingextralifes.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingextralifes.py
@@ -1,36 +1,41 @@
 # Awarding Extra Lifes, Anthony Pitts v0.0
 playerScore = 0
-highScore = 1000
+highScore = 10000
 lives = 3
-high_score = 10000
-highscore = 100000
+name = "Anthony"
+print(f"hello {name}! you scored {highScore} points. \n")
+
+
+
+
+
+# Character Reference
+# Curly Braces {}
+# brackets []
+# angle brackets <>
+# parentheses ()
 
 # Allow the user to input the score.
 
-# if score is 1000 or less
+# if score is 10000 or less
   # Lose a Life
-# If score is > 10000 but less than 10001
+# If score is > 10000 but less than 100001
   # Give one extra life
 # if score is > 100000
   # Give 2 Extra Lifes
 
 # Output the score and number of lives to the screen.
-if highScore <= 1000:
+if highScore <= 10000:
 
-  print("You lost one life \n")
-if high_score >= 10000:
+  print("You lost one life.\n")
+elif highScore >= 10000:
    
-   print("You have gained a new life\n")
-else: ("You have not gained a new life\n")
-if highscore >= 100000:
+   print("You have gained a new life!\n")
+if highScore > 100000:
    
-   print("You have gained two extra lives!\n")
+   print("You have earned two lifes! Lucky Son Of A Gun")
 
-else: print("You did not get the two extra lives\n")
-if playerScore < 1000:
-   
-   print("U lost a life loser!\n")
-   if playerScore >= 10000:
-      print("Congrats you earned a life!\n")
-elif playerScore : <= 10001:
-      print()
+else: ("You have not gained a new life loser!\n")
+
+
+  
