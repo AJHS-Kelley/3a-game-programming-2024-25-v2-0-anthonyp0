bc47e44cda3520383b408f0dfcde8d45e34998c0
anthonyp0 <s11928611@students.duvalschools.org>
2024-09-12 12:04:50 -0400

    v0.1
    trying to fix my elif problem

--- 01_gaming_exercises/00_awarding_extra_lives/awardingextralifes.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingextralifes.py
@@ -1,8 +1,9 @@
 # Awarding Extra Lifes, Anthony Pitts v0.0
-
+playerScore = 0
 highScore = 1000
 lives = 3
 high_score = 10000
+highscore = 100000
 
 # Allow the user to input the score.
 
@@ -15,7 +16,21 @@ high_score = 10000
 
 # Output the score and number of lives to the screen.
 if highScore <= 1000:
-  print("You lost one life")
+
+  print("You lost one life \n")
 if high_score >= 10000:
-   print("You have gained a new life")
-else: 
+   
+   print("You have gained a new life\n")
+else: ("You have not gained a new life\n")
+if highscore >= 100000:
+   
+   print("You have gained two extra lives!\n")
+
+else: print("You did not get the two extra lives\n")
+if playerScore < 1000:
+   
+   print("U lost a life loser!\n")
+   if playerScore >= 10000:
+      print("Congrats you earned a life!\n")
+elif playerScore : <= 10001:
+      print()
