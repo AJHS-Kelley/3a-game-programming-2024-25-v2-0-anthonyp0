a90263fe420394c6ae8b05af36fa58f49427eded
anthonyp0 <s11928611@students.duvalschools.org>
2024-09-18 12:04:13 -0400

    v0.5

--- 01_gaming_exercises/04_rock_paper_scissors/rps.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.py
@@ -8,8 +8,21 @@ playerScore= 0
 playerName="Test Player"
 playerChoice= None
 
-
 # DATA STRUCTURES -- CPU
 cpuScore = 0
 cpuChoice = None
+
+# PLAYER NAME INPUT
+playerName = input("Please type your name and press enter\n")
+print(f"Hello {playerName}!\n")
+isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
+print("isCorrect")
+
+if isCorrect == "yes":
+    print(f"Ok {playerName}, lets play rock, paper, scissors")
+else:
+    playerName = input("Please type your name and press enter\n")
+
+    
+
 
