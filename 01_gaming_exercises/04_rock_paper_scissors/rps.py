ca69b05d024a98ee2e265c580eb42147185b500d
anthonyp0 <s11928611@students.duvalschools.org>
2024-09-18 11:50:47 -0400

    v0.4
    module imports

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.py
@@ -0,0 +1,15 @@
+# Rock, Paper , Scissors by Anthony Pitts, v0.0
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- Player
+playerScore= 0
+playerName="Test Player"
+playerChoice= None
+
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None
+
