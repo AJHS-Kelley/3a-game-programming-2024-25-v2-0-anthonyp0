23ebf5cf4f2229d46e09d9190546076ef3fcfc1d
anthonyp0 <s11928611@students.duvalschools.org>
2024-10-28 13:14:14 -0400

    v0,1
    more functions

--- 00_computer_science_exercises/05_functions/functionsPractice.py
+++ 00_computer_science_exercises/05_functions/functionsPractice.py
@@ -1,7 +1,7 @@
 # Functions Practice, Anthony Pitts, v0.0
 
 import random
-userChoice=None
+
 def helloWorldMulti(): # FUNCTION SIGNATURE
     """This function will output Hello, World! in a language the user choose. """ # docstring \
     # print a list of languages to the screen, at least three but no more than five.
@@ -25,20 +25,30 @@ def helloWorldMulti(): # FUNCTION SIGNATURE
         print("Salut tout le monde!\n")
     else:
         print("Please pick an number.\n")
-    
+        exit()
 
     # print "Hello, World!" to the screen in that language.
-    helloWorldMulti()
-    print(f"English,Spanish,French")
-    input(f"Hey User, pick a language. {userChoice}\n")
+    #helloWorldMulti()
 
-    userChoice = random.randint(0, 3) 
-    if userChoice == 1:
-        print("you have chosen english.\n")
-    elif userChoice == 2:
-        print("you have chosen spanish.\n")
-    elif userChoice == 3:
-        print("you have chosen french.\n")
+
+    # function to determine even / odd numbers
+arguement1=random.randint(-1000,1000)
+
+def isEven(arguement1:int) -> bool: # requires one argument (arguement1) and returns a boolean value.
+    """determines if an integer value is even or odd."""
+    if arguement1 % 2== 0:
+        return True
     else:
-        print("enter a number 1 through 3 to pick an language.\n")
-        exit()
+        False
+
+print(isEven(arguement1)) 
+
+# function with multiple arguements
+def canRideRollerCoaster(age: int, height: int)-> bool:
+    if age > 10 and height > 4:
+        print("you can ride.\n")
+        return True
+    else:
+        print("you cannot ride.\n")
+        return False
+canRideRollerCoaster(4,10) # ARGUEMENTS MUST BE PASSED IN THE SAME ORDER AS THE FUNCTION SIGNATURE INDICATES.
